c6fcca17b5d58d8fb267e9cba07141eb3c789e4c
lgx <lgx@mac.local>
2016-09-10 22:55:29 +0800

    modified:   init.el

--- init.el
+++ init.el
@@ -1,7 +1,22 @@
+
+(defconst data-path "~/.data/emacs/" "")
 (defconst site-path "~/.emacs.d/arms/" "")
 (defconst init-path "~/.emacs.d/init/" "")
-(add-to-list 'load-path init-path)
 
+(setq auto-save-list-file-prefix (concat data-path "auto-save-list"))
+(setq backup-directory-alist `((".*" . , (concat data-path "backup"))))
+(setq auto-save-file-name-transforms `((".*" , (concat data-path "auto-save-list") t)))
+
+(menu-bar-mode -1)
+(setq default-tab-width 4)
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq-default c-basic-offset 4)
+(setq whitespace-global-mode t)
+(setq inhibit-startup-message t)
+(setq initial-scratch-message "")
+(setq-default indent-tabs-mode nil)
+
+(add-to-list 'load-path init-path)
 (require 'igo)
 (require 'iui)
 (require 'iavy)
